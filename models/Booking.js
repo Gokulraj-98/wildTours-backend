71c7f57a3a31ec661d8fa3e30622acbb43de22ff
Gokulraj-98 <148607497+Gokulraj-98@users.noreply.github.com>
2024-06-08 14:24:24 +0530

    Update Booking.js

--- models/Booking.js
+++ models/Booking.js
@@ -1,10 +1,20 @@
 const mongoose = require('mongoose');
 
-const bookingSchema = new mongoose.Schema({
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-    tour: { type: mongoose.Schema.Types.ObjectId, ref: 'Tour' },
-    date: Date,
-});
+const bookingSchema = new mongoose.Schema(
+    {
+        customerName: { type: String, required: true },
+        customerPhoneNumber: { type: Number, required: true },
+        totalAmount: { type: Number, required: true },
+        //calculate tax => 2 % => ((2 *totalAmount)/100)
+        tax: { type: Number, required: true },
+        subTotal: { type: Number, required: true },
+        paymentMode: { type: String, required: true },
+        cartItems: { type: Array, required: true }
+
+    },
+    { timestamps: true }
+
+);
 
 const Booking = mongoose.model('Booking', bookingSchema);
 module.exports = Booking;
