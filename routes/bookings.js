ed4661c22782b7c8f61af1440eb4f33e1a94f012
Gokulraj-98 <148607497+Gokulraj-98@users.noreply.github.com>
2024-06-08 14:28:48 +0530

    Update bookings.js

--- routes/bookings.js
+++ routes/bookings.js
@@ -4,10 +4,20 @@ const auth = require('../middleware/auth');
 
 const router = express.Router();
 
-router.post('/', auth, async (req, res) => {
-    const booking = new Booking({ ...req.body, user: req.user._id });
+router.post("/charge-bill", auth, async (req, res) => {
+    const booking = new Booking(req.body);
     await booking.save();
+    res.send("Bill saved successfully")
     res.status(201).send(booking);
 });
+
+router.get("/get-bill", async (req, res) => {
+    try {
+        const bills = await Booking.find()
+        res.send(bills)
+    } catch (error) {
+        res.status(400).json(error)
+    }
+})
 
 module.exports = router;
